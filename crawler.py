e6c7fa213ee979eb96eb6734b4cdb0289d87f2b6
Slavkata <slavkirilov00@gmail.com>
2020-09-28 13:39:17 +0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,22 +6,17 @@ from time import sleep
 def request_page(url):
     response = requests.get(url).text
 
-    label = re.findall(r'<td bgcolor=".+" class="cell">[\w&;"></=\s]+</td>', response)[:18]
-    append = [re.search(r'<span class="tiny">(\w+)</span>', l).group(1) for l in label]
+    label = re.findall('<td bgcolor=".+" class="cell">[\w&;"></=\s]+</td>', response)[:18]
+    append = [re.search('<span class="tiny">(\w+)</span>', l).group(1) for l in label]
 
-    label = [re.search(r'(\d+)&thinsp;', l).group(1) for l in label]
-
-    label = [label[0] + append[0] for i in range(0, len(label))]
-
+    label = [re.search('(\d+)&thinsp;', l).group(1) for l in label]
     for i in range(0, len(label)):
         label[i] += append[i]
 
-    temp = re.findall(r'<span class="temp">(\d+)</span>', response)[:18]
+    temp = re.findall('<span class="temp">(\d+)</span>', response)[:18]
     
     res = [{'time': label[i], 'temp': temp[i]} for i in range(len(label))]
-
     for item in res:
-        print(item)
         requests.post(url='http://localhost:5000/weather/', data=item)
 
     return res
